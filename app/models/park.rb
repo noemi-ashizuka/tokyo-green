1e19564f852f2e7fae9d707f23553f657429bc13
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-06-24 10:22:58 +0900

    added attractions to park

--- app/models/park.rb
+++ app/models/park.rb
@@ -2,5 +2,6 @@ class Park < ApplicationRecord
   validates :name, :address, presence: true
   belongs_to :admin, class_name: "User", foreign_key: "user_id"
   
-  FACILITIES = ["toilets", "tennis court", "wheelchair accessible", "baby changing", "picnic area", "drink stand", "cafe", "soccer field", "baseball field"]
+  FACILITIES = ["toilets", "tennis court", "wheelchair accessible", "baby changing", "picnic area", "drink stand", "cafe", "soccer field", "baseball field", "open air stage"]
+  ATTRACTIONS = ["large lawn", "boat riding", "historical site", "nature reserve", "bird watching", "cherry blossoms"]
 end
