0e79bf346bd801394180469124fad22dd94efaa1
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-06-24 10:37:32 +0900

    added best season selection

--- app/models/park.rb
+++ app/models/park.rb
@@ -4,4 +4,5 @@ class Park < ApplicationRecord
   
   FACILITIES = ["toilets", "tennis court", "wheelchair accessible", "baby changing", "picnic area", "drink stand", "cafe", "soccer field", "baseball field", "open air stage"]
   ATTRACTIONS = ["large lawn", "boat riding", "historical site", "nature reserve", "bird watching", "cherry blossoms"]
+  BEST_SEASON = ["summer", "autumn", "spring", "winter"]
 end
