4d5f3c3b8248cee28ac4729fab4532c6b0422277
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-06-25 17:23:02 +0900

    skip auth before index and show

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,4 +1,5 @@
 class ParksController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   skip_after_action :verify_policy_scoped, only: [:index, :show]
 
   def index
