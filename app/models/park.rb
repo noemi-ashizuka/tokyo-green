03ca6532f549d5ef936f7b64beaaab889c31b8a8
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-06-28 14:45:44 +0900

    displaying info on show page

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,6 +1,8 @@
 class Park < ApplicationRecord
   validates :name, :address, presence: true
   belongs_to :admin, class_name: "User", foreign_key: "user_id"
+
+  acts_as_favoritable
   
   FACILITIES = ["toilets", "tennis court", "wheelchair accessible", "baby changing", "picnic area", "drink stand", "cafe", "soccer field", "baseball field", "open air stage"]
   ATTRACTIONS = ["large lawn", "boat riding", "historical site", "nature reserve", "bird watching", "cherry blossoms"]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   has_many :parks
   enum role: [:user, :admin]
   after_initialize :set_default_role, :if => :new_record?
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  acts_as_favoritor
   
   def set_default_role
     self.role ||= :user
