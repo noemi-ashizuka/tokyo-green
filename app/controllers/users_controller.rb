09186c2ee191ad031f57e49f680dea9347b07aee
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-07-02 13:54:00 +0900

    added favorite parks to user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @favs = @user.all_favorites
+    @parks = []
+    @favs.each do |fav|
+      @parks << Park.find(fav.favoritable_id)
+    end
     authorize @user
   end
 end
