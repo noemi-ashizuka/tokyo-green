f2c6dcdc8b2ca729baac660eb13a3963a5422cb4
Noemi Ashizuka <noemi.calanducci@gmail.com>
2021-02-24 15:06:47 +0900

    map and markers working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import { initMapbox } from '../plugins/init_mapbox';
 
 
 // Internal imports, e.g:
@@ -32,7 +33,7 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  
+  initMapbox();
 });
 
 import "controllers"
